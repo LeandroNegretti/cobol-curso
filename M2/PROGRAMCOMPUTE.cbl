7a11e9ca9e95fbfccffb303f170289434777ac6e
LeandroNegretti <negrettileandro3@gmail.com>
2025-12-28 17:05:47 -0300

    Comando Compute

---
+++ M2/PROGRAMCOMPUTE.cbl
@@ -0,0 +1,27 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PROGRAMCOMPUTE.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       77 WS-RESULT                         PIC 9(05) VALUE ZEROS.
+       77 WS-NUM-1                          PIC 99    VALUE ZEROS.
+       77 WS-NUM-2                          PIC 99    VALUE ZEROS.
+       PROCEDURE DIVISION.
+
+           COMPUTE WS-RESULT = 5 + 5
+           COMPUTE WS-RESULT = WS-RESULT * 3
+           COMPUTE WS-RESULT = WS-RESULT - 15
+           COMPUTE WS-RESULT = WS-RESULT / 3
+           COMPUTE WS-RESULT = WS-RESULT ** 2
+
+           DISPLAY "RESULTADO: " WS-RESULT
+           DISPLAY "INFORME O PRIMEIRO NUMERO: "
+           ACCEPT WS-NUM-1
+
+           DISPLAY "INFORME O SEGUNDO NUMERO: "
+           ACCEPT WS-NUM-2
+
+           COMPUTE WS-RESULT = WS-NUM-1 + WS-NUM-2
+           STOP RUN.
+
+       END PROGRAM PROGRAMCOMPUTE.
